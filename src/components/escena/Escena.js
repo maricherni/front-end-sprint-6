453b8f8388bcb7593fa8ea91108715400e334765
Maria Rodriguez <mary_cherny@msn.com>
2022-06-15 17:04:18 +0200

    Excercici1

---
+++ src/components/escena/Escena.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Escena = () => <div> <p> "El nostre heroi estava surant per l'espai sideral quan a la llunyania va albirar una nau espacial"</p> </div>;
+
+export default Escena;
+
+
+/* Escena() {
+  const miConstante = 
+  <div>
+    <p>El nostre heroi estava surant per l'espai sideral quan a la llunyania va albirar una nau espacial</p>
+  </div>;
+
+  return (
+    <div>
+      (miConstante)
+    </div>
+  );
+} */
