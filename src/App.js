ceea49d2cca85ea9c3089f408dff74887aa4383c
Maria Rodriguez <mary_cherny@msn.com>
2022-06-30 15:54:58 +0200

    Exercici4

--- src/App.js
+++ src/App.js
@@ -1,18 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
-import Escena from "./components/escena/Escena";
-import { textos } from './components/escena/Textos';
-
+import React from 'react';
+import {Escena, Boton} from "./components/escena/Escena";
+import {Textos} from './components/escena/Textos';
 
 function App() {
+  const [frases, setFrases] = React.useState(Textos)
+  
+  const frasesHistoria = frases.map(frase => (
+    <div key={frase.text}>
+      <Escena 
+        texto = {frase.text} 
+        active = {frase.active}
+      />
+    </div>
+  ))
+
+ function isActivePrev(){
+    const activeIndex = frases.findIndex(frase => frase.active); //Buscar índice de frase activa
+        activeIndex === 0? frases[activeIndex].active = true : frases[activeIndex].active = false; // desactivar la frase actual, pero si está en primera posición se queda activada
+    const prevText = activeIndex === 0 ? frases[activeIndex]: frases[activeIndex-1]; 
+        prevText.active = true; // activar la frase anterior 
+        console.log(frases[activeIndex], prevText)
+  }
+
+  const isActiveNext = ()=> {
+      const activeIndex = frases.findIndex(frase => frase.active);
+        activeIndex === frases.length-1? frases[activeIndex].active = true : frases[activeIndex].active = false; // desactivar la frase actual, pero si está en última posición se queda activada
+      const nextText = activeIndex === frases.length-1? frases[activeIndex]: frases[activeIndex+1]; 
+        nextText.active = true; // activar la frase siguiente     
+        console.log(frases[activeIndex], nextText)
+  }
+
   return (
    <div>
-    <Escena frase = {textos[0]}/>
-    <Escena frase = {textos[1]}/>
-    <Escena frase = {textos[2]}/>
-    <Escena frase = {textos[3]}/>
-    
-   </div> 
+    <div className="buttons">
+      <button onClick={isActivePrev}><Boton textoBoton = 'Anterior'/></button>
+      <button onClick={isActiveNext}><Boton textoBoton = 'Següent'/></button>
+    </div>
+    <div>{frasesHistoria}</div>
+    </div> 
    
   )
 };
--- src/components/escena/Escena.js
+++ src/components/escena/Escena.js
@@ -1,20 +1,16 @@
 import React from "react";
+import App from "../../App";
+import { BordesTexto, Botones, ActiveText} from './Styled';
 
+//Botones anterior & següent
+export const Boton = (props) =>   
+  <Botones><div>{props.textoBoton}</div></Botones>
 
-const Escena = (props) => <div> <p>{props.frase}</p> </div>;
-
-export default Escena;
-
-
-/* Escena() {
-  const miConstante = 
-  <div>
-    <p>El nostre heroi estava surant per l'espai sideral quan a la llunyania va albirar una nau espacial</p>
-  </div>;
-
+//Texto historia. Si la propiedad "active" es "true" se pondrá de color rosa y si es "false" se quedará blanco.
+export function Escena (props) {
+  const [active, setActive] = React.useState(props.active);
   return (
-    <div>
-      (miConstante)
-    </div>
-  );
-} */
+    active?<div> <ActiveText><p>{props.texto}</p></ActiveText></div> : <div><BordesTexto><p>{props.texto}</p></BordesTexto></div> 
+  )
+  }
+
