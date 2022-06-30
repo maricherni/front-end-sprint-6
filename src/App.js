ea85c5f501bbefa07c82eb50b62ce4cc32b505b3
Maria Rodriguez <mary_cherny@msn.com>
2022-06-30 17:48:37 +0200

    Exercici 5

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import React from 'react';
+import {useState} from 'react';
 import {Escena, Boton} from "./components/escena/Escena";
 import {Textos} from './components/escena/Textos';
+import {Center} from './components/escena/Styled.js';
 
 function App() {
-  const [frases, setFrases] = React.useState(Textos)
+  const [frases, setFrases] = useState(Textos)
   
   const frasesHistoria = frases.map(frase => (
     <div key={frase.text}>
@@ -20,10 +22,10 @@ function App() {
         activeIndex === 0? frases[activeIndex].active = true : frases[activeIndex].active = false; // desactivar la frase actual, pero si está en primera posición se queda activada
     const prevText = activeIndex === 0 ? frases[activeIndex]: frases[activeIndex-1]; 
         prevText.active = true; // activar la frase anterior 
-        console.log(frases[activeIndex], prevText)
+        console.log(frases[activeIndex], prevText) 
   }
 
-  const isActiveNext = ()=> {
+  function isActiveNext () {
       const activeIndex = frases.findIndex(frase => frase.active);
         activeIndex === frases.length-1? frases[activeIndex].active = true : frases[activeIndex].active = false; // desactivar la frase actual, pero si está en última posición se queda activada
       const nextText = activeIndex === frases.length-1? frases[activeIndex]: frases[activeIndex+1]; 
@@ -31,15 +33,31 @@ function App() {
         console.log(frases[activeIndex], nextText)
   }
 
+  //RENDERIZADO CONCIDIONAL. Para que aparezca una página de inicio antes de la historia.
+  let [pagInicial, setPagInicial] = useState(true);
+
+
   return (
-   <div>
+  <div>
+    {pagInicial && 
+     <div>
+        <Center><h1>Una història d'herois</h1></Center>
+        <Center><p>A continuació t'endinsaràs en una gran història d'herois. </p>
+                <p>Trobaràs dos botons que et permetran avançar i retrocedir en la història. Posa't còmode i gaudeix.</p></Center>
+        <Center><button onClick={()=> setPagInicial(pagInicial === true? pagInicial = false : pagInicial = true)}><Boton textoBoton = 'Començar història'/></button></Center>
+      </div>
+    }
+    {!pagInicial &&
     <div className="buttons">
       <button onClick={isActivePrev}><Boton textoBoton = 'Anterior'/></button>
       <button onClick={isActiveNext}><Boton textoBoton = 'Següent'/></button>
-    </div>
-    <div>{frasesHistoria}</div>
     </div> 
-   
+    }
+    {!pagInicial &&
+      <div>{frasesHistoria}</div>
+    }
+  </div> 
+  
   )
 };
 
